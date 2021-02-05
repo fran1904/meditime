e06710dd5d2cc4a6879179326fc1267e27d965b8
aos-jamerza <awsjamerza@gmail.com>
2021-02-05 18:22:18 +0100

    Neue-medikament

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.dbUri, { useNewUrlParser: true, useUnifiedTopology:
     .catch(err => console.log(err))
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: false}))
+app.use(express.urlencoded({ extended: true}))
 app.use(methodOverride('_method'))
 app.use(express.static('./public'))
 app.set('view engine', 'ejs');   
@@ -42,7 +42,6 @@ app.get('/login', (req, res) => {
     res.render('login')
 })
 
-
 // Load the index-of-docs page. This is an index page that loops through the DB documents, e.g. blogposts, quotes
 app.get('/medikamente', (req, res) => {
     Medicine.find()
@@ -52,7 +51,6 @@ app.get('/medikamente', (req, res) => {
         })
         .catch(err => console.log(err))
 })
-
 
 // Load the index-of-docs page. This is an index page that loops through the DB documents, e.g. blogposts, quotes
 app.get('/therapie', (req, res) => {
@@ -75,62 +73,44 @@ app.get('/therapie', (req, res) => {
 
 // Load the 'create-single-doc' page. The view contains a form with which the user can create a new single document in the database.
 app.get('/', (req, res) => {
+    // console.log(Medicine)
     Medicine.find()
     .then(result => {
-        res.render('medikamente', {Medicine: result})
+        res.render('medikamente', {Medicines: result})
     })
     .catch(err => console.log(err) )
 })
 
-////// die brauche nicht 
-// app.get('/neue-medikament', (req, res) => {
-//     Medicine.aggregate([{ $sample: { size: 6 } }])
-//     .then(result => {
-//         res.render('neue-medikament', {Medicine: result})
-//     })
-// })
-
-// Create a new DB entry from the frontend with POST
-// app.post('/neue-medikament', (req, res) => {
-//     const Medicine = new Medicine(req.body)
-
-//     Medicine.save()
-//         .then(result => {
-//             res.redirect('medikamente')
-//         })
-//         .catch(err => console.log(err))
-// })
-///// bis hier
-
-
-
-
-
-
-
-
 app.get('/neue-medikament', (req, res) => {
     // console.log("hallo")
-    Medicine.aggregate([{ $sample: { size: 6 } }])
+    Medicine.aggregate([{ $sample: { size: 30 } }])
     .then(result => {
-        res.render('neue-medikament', {Medicine: result})
+        res.render('neue-medikament', {Medicines: result})
     })
 })
 
 
-
-
-
-//Creates a new DB entry from the frontend with POST
+// Creates a new DB entry from the frontend with POST
 app.post('/neue-medikament', (req, res) => {
     
-    const Medicine = new Medicine(req.body)
+    const Medicines = new Medicine(req.body)
 
-    Medicine.save()
+    Medicines.save()
         .then(result => {
-            res.redirect('medikamentes')
+            console.log(req.body)
+            res.redirect('/medikamente')
         })
         .catch(err => console.log(err))
+})
+
+app.get('/single/:medikamentId',(req,res) => {
+    // console.log(req.params.MedikamentId)
+    // console.log(params)
+    Medicines.findById(req.params.MedikamentId)
+    .then(result => {
+        res.render('medikamente', {Medikament: result})
+    })
+    .catch(err => console.log(err))
 })
 
 
